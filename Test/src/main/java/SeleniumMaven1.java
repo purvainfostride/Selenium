090c637fce9690259f6623e57191d290b720f437
purvainfostride <purva@infostride.com>
2022-01-18 21:05:39 +0530

    Selenium Maven basics

--- Test/src/main/java/SeleniumMaven1.java
+++ Test/src/main/java/SeleniumMaven1.java
@@ -15,34 +15,40 @@ public class SeleniumMaven1 {
         WebDriverManager.chromiumdriver().setup();
         WebDriver driver = new ChromeDriver();
         Actions actions = new Actions(driver);
-        String userName = "Aatimverma";
+        String userName = "Cassidy Hope";
 
-        //.........................................................BROWSER ACTIONS
+       //................................................... ........LOGIN....................
         driver.get("https://opensource-demo.orangehrmlive.com/index.php/dashboard"); //to get to this page
         driver.manage().window().maximize();
         driver.findElement(By.name("txtUsername")).sendKeys("Admin");
         driver.findElement(By.name("txtPassword")).sendKeys("admin123");
         driver.findElement(By.name("Submit")).click();
-       WebElement admin = driver.findElement(By.id("menu_admin_viewAdminModule"));
+        //................................................... ..................................
+        WebElement admin = driver.findElement(By.id("menu_admin_viewAdminModule"));
         actions.moveToElement(admin).perform();
-
         WebElement users= driver.findElement(By.id("menu_admin_UserManagement"));
         actions.moveToElement(users).perform();
         WebElement userButton= driver.findElement(By.id("menu_admin_viewSystemUsers"));
         userButton.click();
-        /* driver.findElement(By.name("btnAdd")).click();
-        driver.findElement(By.name("systemUser[employeeName][empName]")).sendKeys("Aatim verma");
+          driver.findElement(By.name("btnAdd")).click();
+        driver.findElement(By.name("systemUser[employeeName][empName]")).sendKeys("Cassidy Hope");
         driver.findElement(By.id("systemUser_userName")).sendKeys(userName);
         driver.findElement(By.name("systemUser[password]")).sendKeys("Hello*#@1234");
         driver.findElement(By.name("systemUser[confirmPassword]")).sendKeys("Hello*#@1234");
         WebElement saveButton = driver.findElement(By.xpath("//input[@value='Save']"));
         Thread.sleep(2000);
-        saveButton.click(); */
+        saveButton.click();
         driver.findElement(By.name("searchSystemUser[userName]")).sendKeys(userName);
         driver.findElement(By.id("searchBtn")).click();
        /*   String searchedUsernameXpath = "//a[contains(text(),'%s')]";
        WebElement searchedUsername = driver.findElement(By.xpath(String.format(searchedUsernameXpath, userName)));
         Assert.isTrue(searchedUsername.getText().equals(userName),"Username Not found");  */
+        //................................................... ........LOGOUT....................
+         /*   driver.findElement(By.id("welcome")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.partialLinkText("Logout")).click(); */
+      //  driver.findElement(By.id("btnDelete")).sendKeys("Aatim9");
+
 
 
 
