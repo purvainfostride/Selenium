e8b60cf757c0c911e73964b41a4a245c0054c8f4
purvainfostride <purva@infostride.com>
2022-01-20 17:45:28 +0530

    Multilevel Inheritance in login,menuAdmin,add,logout and search,delete user, menuRecruit, menuPim

--- Test/src/main/java/MultilevelInherit.java
+++ Test/src/main/java/MultilevelInherit.java
@@ -1,11 +1,21 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
- class parent{
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
+
+import java.util.Random;
+
+class parent{
      WebDriver driver = new ChromeDriver();
-     String userName = "Cassidy Dope5";
+     Random random = new Random();
+     int x = random.nextInt(60);
+     String userName = "userno"+x;
+
     void login() {
         driver.get("https://opensource-demo.orangehrmlive.com/index.php/dashboard"); //to get to this page
         driver.manage().window().maximize();
@@ -13,25 +23,50 @@ import org.openqa.selenium.chrome.ChromeDriver;
         driver.findElement(By.name("txtPassword")).sendKeys("admin123");
         driver.findElement(By.name("Submit")).click();
     }
-    void addUser() throws InterruptedException{
+    void test() {
+     /* driver.get(driver.getCurrentUrl());
+        String expectedTitle ="OraangeHRM";
+        String actualTitle =driver.getTitle();
+        //It will only show the message if it will fail and also rest of the functions will not execute as well
+        Assert.assertEquals(actualTitle,expectedTitle,"Title");*/
+        SoftAssert softAssert= new SoftAssert();
+        driver.get(driver.getCurrentUrl());
+        String expectedTitle ="OraangeHRM";
+        String actualTitle =driver.getTitle();
+        softAssert.assertEquals(actualTitle,expectedTitle,"Title" );
+        softAssert.assertNotEquals(actualTitle, "Most Reliable App & Cross Browser Testing Platform | BrowserStack");
+        //softAssert.assertNull(expectedTitle);  Always fails
+        softAssert.assertNotNull(expectedTitle);
+      //  softAssert.assertAll();// to display in console that which test cases failed
+    }
+    void menuAdmin(){
         WebElement admin = driver.findElement(By.id("menu_admin_viewAdminModule"));
         admin.click();
         WebElement users = driver.findElement(By.id("menu_admin_UserManagement"));
         users.click();
         WebElement userButton = driver.findElement(By.id("menu_admin_viewSystemUsers"));
         userButton.click();
+    }
+   void addUser() throws InterruptedException{
+
         driver.findElement(By.name("btnAdd")).click();
-        driver.findElement(By.name("systemUser[employeeName][empName]")).sendKeys("Cassidy Hope");
+        driver.findElement(By.name("systemUser[employeeName][empName]")).sendKeys("Ananya Dash");
         driver.findElement(By.id("systemUser_userName")).sendKeys(userName);
         driver.findElement(By.name("systemUser[password]")).sendKeys("Hello*#@1234");
         driver.findElement(By.name("systemUser[confirmPassword]")).sendKeys("Hello*#@1234");
         WebElement saveButton = driver.findElement(By.xpath("//input[@value='Save']"));
         Thread.sleep(2000);
         saveButton.click();
-    }
+      }
+          /* void edit() throws InterruptedException{
+              //driver.findElement(By.partialLinkText("Admin")).click();
+              WebElement editUser = driver.findElement(By.xpath("//*[@id=\"resultTable\"]/tbody/tr[1]/td[2]/a"));
+              Thread.sleep(2000);
+            editUser.click();
+          } */
     void searchUser() throws InterruptedException{
         Thread.sleep(2000);
-        driver.findElement(By.name("searchSystemUser[userName]")).sendKeys("Cassidy Dope2");
+        driver.findElement(By.name("searchSystemUser[userName]")).sendKeys("userno30");
         driver.findElement(By.id("searchBtn")).click();
     }
     void deleteUser()throws InterruptedException{
@@ -40,22 +75,57 @@ import org.openqa.selenium.chrome.ChromeDriver;
         driver.findElement(By.name("btnDelete")).click();
         driver.findElement(By.id("dialogDeleteBtn")).click();
     }
-    void logoutUser () throws InterruptedException{
+    void menuRecruit(){
+        WebElement recruit= driver.findElement(By.xpath("//*[contains(@id,'menu_recruitment_viewRecruitmentModule')]"));
+        recruit.click();
+        //..........................................DROP DOWN MENU- Job TITLE
+        Select jobTitle= new Select(driver.findElement(By.id("candidateSearch_jobTitle")));
+        jobTitle.selectByVisibleText("BTest");
+        //..........................................DROP DOWN MENU- Status
+        Select status= new Select(driver.findElement(By.id("candidateSearch_status")));
+        status.selectByValue("SHORTLISTED");
+        //.........................................Calender-Date of Application
+        driver.findElement(By.id("candidateSearch_fromDate")).click();
+        driver.findElement(By.id("candidateSearch_fromDate")).sendKeys("2022-01-13");
+        driver.findElement(By.id("candidateSearch_fromDate")).sendKeys(Keys.ENTER);
+        //.........................................Calender-To
+        driver.findElement(By.id("candidateSearch_toDate")).click();
+        driver.findElement(By.id("candidateSearch_toDate")).sendKeys("2022-02-13");
+        driver.findElement(By.id("candidateSearch_toDate")).sendKeys(Keys.ENTER);
+    }
+    void menuPim(){
+        WebElement pim= driver.findElement(By.xpath("//*[contains(@id,'menu_pim_viewPimModule')]"));
+        pim.click();
+        driver.findElement(By.id("menu_pim_addEmployee")).click();
+        WebElement photograph= driver.findElement(By.xpath("//*[contains(@id,'photofile')]"));
+        photograph.sendKeys("C:\\Users\\Purva\\Downloads\\download.png");
+      photograph.click();
+
+    }
+  /* void logoutUser () throws InterruptedException{
         driver.findElement(By.id("welcome")).click();
         Thread.sleep(2000);
         driver.findElement(By.partialLinkText("Logout")).click();
-    }
- }
+    }*/
+
+
+}
 
 public class MultilevelInherit extends parent{
     public static void main(String[] args)  throws InterruptedException {
         WebDriverManager.chromiumdriver().setup();
         MultilevelInherit I = new MultilevelInherit ();
         I.login();
+        I.test();
+        I.menuAdmin();
         I.addUser();
+        // I.edit();
         I.searchUser();
         I.deleteUser();
-        I.logoutUser();
+        I.menuRecruit();
+        I.menuPim();
+      //  I.logoutUser();
+
 
     }
 }
--- Test/src/main/java/SeleniumMaven1.java
+++ Test/src/main/java/SeleniumMaven1.java
@@ -1,15 +1,15 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import net.jodah.failsafe.internal.util.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+
+import java.security.Key;
 import java.time.Duration;
 import java.io.File;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.Select;
+
 
 public class SeleniumMaven1 {
     public static void main(String[] args)  throws InterruptedException {
@@ -26,13 +26,13 @@ public class SeleniumMaven1 {
         driver.findElement(By.name("txtPassword")).sendKeys("admin123");
         driver.findElement(By.name("Submit")).click();
         //................................................... ..................................
-        WebElement admin = driver.findElement(By.id("menu_admin_viewAdminModule"));
+        /*  WebElement admin = driver.findElement(By.id("menu_admin_viewAdminModule"));
         actions.moveToElement(admin).perform();
         WebElement users= driver.findElement(By.id("menu_admin_UserManagement"));
         actions.moveToElement(users).perform();
         WebElement userButton= driver.findElement(By.id("menu_admin_viewSystemUsers"));
         userButton.click();
-        //.........................................................Adding user
+       //.........................................................Adding user
         driver.findElement(By.name("btnAdd")).click();
         driver.findElement(By.name("systemUser[employeeName][empName]")).sendKeys("Cassidy Dope");
         driver.findElement(By.id("systemUser_userName")).sendKeys(userName);
@@ -47,20 +47,21 @@ public class SeleniumMaven1 {
         //..........................................................Searching user
         driver.findElement(By.name("searchSystemUser[userName]")).sendKeys("demo1");
         driver.findElement(By.id("searchBtn")).click();
-       /*   String searchedUsernameXpath = "//a[contains(text(),'%s')]";
+   String searchedUsernameXpath = "//a[contains(text(),'%s')]";
        WebElement searchedUsername = driver.findElement(By.xpath(String.format(searchedUsernameXpath, userName)));
         Assert.isTrue(searchedUsername.getText().equals(userName),"Username Not found");  */
         //................................................... ........LOGOUT....................
          /*   driver.findElement(By.id("welcome")).click();
         Thread.sleep(2000);
-        driver.findElement(By.partialLinkText("Logout")).click(); */
+        driver.findElement(By.partialLinkText("Logout")).click();
 
        // Thread.sleep(2000);
        // driver.close();
         //................................................... ........DELETE....................
         driver.findElement(By.id("ohrmList_chkSelectRecord_54")).click();
         driver.findElement(By.name("btnDelete")).click();
-        driver.findElement(By.id("dialogDeleteBtn")).click();
+        driver.findElement(By.id("dialogDeleteBtn")).click(); */
+        //.....................................................................................
 
 
 
