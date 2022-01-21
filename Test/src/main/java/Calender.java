b8a45b6af0238af3291c546442c491d915b05529
purvainfostride <purva@infostride.com>
2022-01-21 22:03:14 +0530

    Multilevel Inheritance in login,menuAdmin,add,logout and search,delete user, menuRecruit, menuPim

--- Test/src/main/java/Calender.java
+++ Test/src/main/java/Calender.java
@@ -1,11 +1,14 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import java.util.List;
+import org.openqa.selenium.support.ui.Select;
 
+import java.util.List;
+import java.util.Scanner;
+import java.text.SimpleDateFormat;
 
 public class Calender {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         WebDriverManager.chromiumdriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://opensource-demo.orangehrmlive.com/index.php/dashboard");
@@ -13,9 +16,20 @@ public class Calender {
         driver.findElement(By.name("txtUsername")).sendKeys("Admin");
         driver.findElement(By.name("txtPassword")).sendKeys("admin123");
         driver.findElement(By.name("Submit")).click();
-        WebElement recruit= driver.findElement(By.xpath("//*[contains(@id,'menu_recruitment_viewRecruitmentModule')]"));
+
+        WebElement recruit = driver.findElement(By.xpath("//*[contains(@id,'menu_recruitment_viewRecruitmentModule')]"));
         recruit.click();
-        driver.findElement(By.id("candidateSearch_fromDate")).click();
+        driver.findElement(By.xpath("//*[contains(@id,'candidateSearch_fromDate')]")).click();
+        Select calMonth = new Select(driver.findElement(By.className("ui-datepicker-month")));
+        calMonth.selectByValue("1");
+        Select calYear = new Select(driver.findElement(By.className("ui-datepicker-year")));
+        calYear.selectByValue("2022");
+        while (!(calMonth.equals("Jun") && calYear.equals("2022"))) {
+            driver.findElement(By.xpath("//a[@title='Next']")).click();
+            calMonth.selectByValue("1");
+            calYear.selectByValue("2022");
+        }
+        driver.findElement(By.xpath("//a[text()='23']")).click();
 
 
     }
